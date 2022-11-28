a5f43ada672b00c38fe30945152d434545b14309
duihuhu <duihuhu@163.com>
2022-11-28 20:21:59 +0800

    debug remote get object

--- example.py
+++ example.py
@@ -1,21 +1,37 @@
 import ray
 import time
 import os
+import numpy as np
 ray.init()
 
 @ray.remote
-def circle(x):
-    return x*x
-node_id = 'f2c09c50dae154540c47d9c78d6f38b7a6c2f1af003676e3fa140c77'
-node_bytes = bytes.fromhex(node_id)
-print(node_bytes)
-futures = 1
+def circle():
+    return np.zeros(10000)
+
+head_id = ray.get_runtime_context().node_id.hex()
+
+# print(ray.state.node_ids())
+remote_node_id = ""
+nodes = ray.nodes()
+for node in nodes:
+    n_id = node['NodeID']
+    if n_id != head_id:
+        remote_node_id = n_id
+
+
+# node_id = 'b0ef226853852bde98e2c1874425e0a361dace6a5fa180e19841eee4'
+remote_node_bytes = bytes.fromhex(remote_node_id)
+# # print(node_bytes)
+# futures = 1
 c = circle.options(
     scheduling_strategy=ray.util.scheduling_strategies.NodeAffinitySchedulingStrategy(
         #node_id = ray.get_runtime_context().node_id,
-        node_id = node_bytes,
+        node_id = remote_node_bytes,
         soft = False
     )
-).remote(futures)
+).remote()
 
 print(ray.get(c))
+
+print(c.__sizeof__())
+
