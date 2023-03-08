3b2bafe6f47a43c1288da95ed504cf7fc4f49e0e
duihuhu <duihuhu@163.com>
2023-03-08 16:20:22 +0800

    debug add test iops

--- mul_example.py
+++ mul_example.py
@@ -15,6 +15,7 @@ def dircle():
     return np.zeros(100000)
 
 def worker(barrier, lock):
+  ray.init(address='auto', _node_ip_address='192.172.200.2')
   head_id = ray.get_runtime_context().node_id.hex()
   # print(ray.state.node_ids())
   remote_node_id = ""
