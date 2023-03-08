8a95a2d8650586a7a7fe5cf8380ecfb41883dd60
duihuhu <duihuhu@163.com>
2023-03-08 19:12:54 +0800

    debug add test iops

--- test_size.py
+++ test_size.py
@@ -1,8 +1,5 @@
 import numpy as np
-def dircle():
-    return np.zeros(10000000)
-    #return np.zeros(100000)
 
-a = dircle()
-with open("buffer.txt", 'a+') as fd:
-    fd.write(str(a))
+import numpy as np
+data =  np.zeros(10000000)
+print ('%d bytes'%(data.size * data.itemsize))
