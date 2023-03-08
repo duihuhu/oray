b84ef98dc9823d22b2a06a3a7e2e6dcf8cda8a3b
duihuhu <duihuhu@163.com>
2023-03-08 18:33:54 +0800

    debug add test iops

--- mul_example.py
+++ mul_example.py
@@ -16,25 +16,25 @@ def dircle():
 
 def worker(barrier):
   ray.init(address='auto', _node_ip_address='192.172.200.2')
-  head_id = ray.get_runtime_context().node_id.hex()
-  # print(ray.state.node_ids())
-  remote_node_id = ""
-  nodes = ray.nodes()
-  for node in nodes:
-      n_id = node['NodeID']
-      if n_id != head_id:
-          remote_node_id = n_id
+  # head_id = ray.get_runtime_context().node_id.hex()
+  # # print(ray.state.node_ids())
+  # remote_node_id = ""
+  # nodes = ray.nodes()
+  # for node in nodes:
+  #     n_id = node['NodeID']
+  #     if n_id != head_id:
+  #         remote_node_id = n_id
 
-  remote_node_bytes = bytes.fromhex(remote_node_id)
+  # remote_node_bytes = bytes.fromhex(remote_node_id)
 
 
-  d = dircle.options(
-      scheduling_strategy=ray.util.scheduling_strategies.NodeAffinitySchedulingStrategy(
-          #node_id = ray.get_runtime_context().node_id,
-          node_id = remote_node_bytes,
-          soft = False
-      )
-  ).remote()
+  # d = dircle.options(
+  #     scheduling_strategy=ray.util.scheduling_strategies.NodeAffinitySchedulingStrategy(
+  #         #node_id = ray.get_runtime_context().node_id,
+  #         node_id = remote_node_bytes,
+  #         soft = False
+  #     )
+  # ).remote()
   barrier.wait()
   time.sleep(30)
   # with lock:
@@ -42,7 +42,7 @@ def worker(barrier):
 
   # time.sleep(10)
   t1 = time.time()
-  e = ray.get(d)
+  # e = ray.get(d)
   t2=time.time()
   print("time: " , t1, " ", t2, " ", t2-t1)
   print(e)
