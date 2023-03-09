2d465b99cf547f8d39184a150ee081fbed34501e
duihuhu <duihuhu@163.com>
2023-03-09 15:09:54 +0800

    debug add mul request iops test

--- mull_request_iops.py
+++ mull_request_iops.py
@@ -10,7 +10,7 @@ global n = 0
 #@ray.remote
 #def circle():
 #    return np.zeros(1000000)
-process_parallel = 10
+process_parallel = 1
 # print("a")
 # ray.init(address='auto', _node_ip_address='192.172.200.2')
 head_id = ray.get_runtime_context().node_id.hex()
