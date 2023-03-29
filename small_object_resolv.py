8871f7dbda2fb74e0d5efce1b5c4d2a33acf4dac
duihuhu <duihuhu@163.com>
2023-03-29 20:55:00 +0800

    debug small object reslvoe

--- small_object_resolv.py
+++ small_object_resolv.py
@@ -6,6 +6,19 @@ import time
 import multiprocessing
 ray.init(address='auto', _node_ip_address='192.172.200.2')
 
+head_id = ray.get_runtime_context().node_id.hex()
+# print("head_id", head_id)
+# print(ray.state.node_ids())
+remote_node_id = ""
+nodes = ray.nodes()
+for node in nodes:
+    n_id = node['NodeID']
+    if n_id != head_id:
+        remote_node_id = n_id
+
+remote_node_bytes = bytes.fromhex(remote_node_id)
+head_node_bytes = bytes.fromhex(head_id)
+
 @ray.remote
 def square():
   time.sleep(10)
@@ -19,6 +32,12 @@ def circle(ref):
 
 d_ref = square.remote()
 
-c_ref = circle.remote([d_ref])
+c_ref = circle.options(
+    scheduling_strategy=ray.util.scheduling_strategies.NodeAffinitySchedulingStrategy(
+        #node_id = ray.get_runtime_context().node_id,
+        node_id = remote_node_bytes,
+        soft = False
+    )
+).remote([d_ref])
 
 print(ray.get(c_ref))
