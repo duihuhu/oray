8729689d8480d546371e7cee674c87cf631afe59
duihuhu <duihuhu@163.com>
2023-04-13 10:45:43 +0800

    add mul_bug_object_resove

--- mul_big_object_resolv.py
+++ mul_big_object_resolv.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 import multiprocessing
 ray.init(address='auto', _node_ip_address='192.172.200.2')
-
+ref_number = 2
 head_id = ray.get_runtime_context().node_id.hex()
 # print("head_id", head_id)
 # print(ray.state.node_ids())
@@ -25,22 +25,33 @@ def square():
   return np.zeros(1024000)
 
 @ray.remote
-def circle(ref):
+def circle(reference):
   time.sleep(2)
-  t1 = time.time()
-  e = ray.get(ref)
-  t2 = time.time()
-  print("time", t2-t1)
-  print(e)
+  for ref in reference:
+    t1 = time.time()
+    e = ray.get(ref)
+    t2 = time.time()
+    print("time", t2-t1)
+    print(e)
   return np.zeros(100)
 
-d_ref = square.options(
-    scheduling_strategy=ray.util.scheduling_strategies.NodeAffinitySchedulingStrategy(
-        #node_id = ray.get_runtime_context().node_id,
-        node_id = head_node_bytes,
-        soft = False
-    )
-).remote()
+# d_ref = square.options(
+#     scheduling_strategy=ray.util.scheduling_strategies.NodeAffinitySchedulingStrategy(
+#         #node_id = ray.get_runtime_context().node_id,
+#         node_id = head_node_bytes,
+#         soft = False
+#     )
+# ).remote()
+
+
+reference = [ square.options(
+  scheduling_strategy=ray.util.scheduling_strategies.NodeAffinitySchedulingStrategy(
+      #node_id = ray.get_runtime_context().node_id,
+      node_id = head_node_bytes,
+      soft = False
+  )
+).remote() for i in range(ref_number) ]
+
 
 c_ref = circle.options(
     scheduling_strategy=ray.util.scheduling_strategies.NodeAffinitySchedulingStrategy(
@@ -48,6 +59,6 @@ c_ref = circle.options(
         node_id = remote_node_bytes,
         soft = False
     )
-).remote([d_ref])
+).remote([reference])
 
 print(ray.get(c_ref))
