629c3ed2c2a344d823538431f36944a18a039365
duihuhu <duihuhu@163.com>
2023-05-24 21:44:52 +0800

    debug add process log

--- process_log.py
+++ process_log.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 pre_name = 'log'
 for i in [1,2,4,8,16]:
   time_list = []
@@ -6,15 +7,16 @@ for i in [1,2,4,8,16]:
     start_time = 1884927212
     end_time = -1
     filename = "data/"+pre_name+"_"+str(i)+"_"+str(j)+".txt"
-    with open(filename, 'r') as fd:
-      for line in fd.readlines():
-        if 'time:' in line:
-          content = line.split(" ")
-          if start_time > float(content[-7]):
-            start_time = float(content[-7])
-          if end_time < float(content[-4]):
-            end_time = float(content[-4])
-    time_list.append(end_time-start_time)
+    if(os.path.exists(filename)):
+      with open(filename, 'r') as fd:
+        for line in fd.readlines():
+          if 'time:' in line:
+            content = line.split(" ")
+            if start_time > float(content[-7]):
+              start_time = float(content[-7])
+            if end_time < float(content[-4]):
+              end_time = float(content[-4])
+      time_list.append(end_time-start_time)
     # print(end_time, start_time, end_time-start_time)
   print(i, (i*3000*1144000)/(np.median(time_list)*1024*1024))
 
