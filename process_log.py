64ed08fb670c651771f2cdd049b548d4b0b8ef63
duihuhu <duihuhu@163.com>
2023-05-24 19:24:37 +0800

    debug add process log

--- process_log.py
+++ process_log.py
@@ -1,10 +1,21 @@
 import numpy as np
-arr = []
-with open("log.txt", 'r') as fd:
-  for line in fd.readlines():
-    if "circle pid=" in line:
-      content = line.split(" ")
-      arr.append(float(content[-1]))
+pre_name = 'log_'
+for i in [1,2,4,8,16]:
+  start_time = 10000
+  end_time = -1
+  time_list = []
+  for j in range(1,6):
+    filename = "data/"+pre_name+"_"+str(i)+"_"+str(j)+".txt"
+    with open(filename, 'r') as fd:
+      for line in fd.readlines():
+        if time in line:
+          content = line.split(" ")
+          start_time = content[-3]
+          end_time = content[-2]
+          if start_time > float(content[-3]):
+            start_time = float(content[-3])
+          if end_time < float(content[-2]):
+            end_time = float(content[-2])
+    time_list.append(end_time-start_time)
+  print(i, j, np.median(time_list), time_list)
 
-print(len(arr))
-print(np.median(arr))
