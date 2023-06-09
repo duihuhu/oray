2d1bf6f2536c3c36a7b0bc15f619ee2c92f6c102
duihuhu <duihuhu@163.com>
2023-06-09 15:47:37 +0800

    debug add cudf ray actor

--- ray_gpu/cudf_task.py
+++ ray_gpu/cudf_task.py
@@ -1,12 +1,10 @@
 import time
-t1=time.time()
 import cudf
-t2=time.time()
+t1=time.time()
 #url = "https://github.com/plotly/datasets/raw/master/tips.csv"
 #content = requests.get(url).content.decode('utf-8')
 tips_df = cudf.read_csv("tips.csv")
 tips_df['tip_percentage'] = tips_df['tip'] / tips_df['total_bill'] * 100
-t4 = time.time()
 result = tips_df.groupby('size').tip_percentage.mean()
-t3=time.time()
-print(t2-t1, t3-t2, t3-t1, t3-t4)
+t2=time.time()
+print(t2-t1)
