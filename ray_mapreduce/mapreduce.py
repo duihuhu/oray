f2524460596d16914e8a2bd778462140a1e99c18
duihuhu <duihuhu@163.com>
2023-06-09 17:10:39 +0800

    debug add ray map reduce

--- ray_mapreduce/mapreduce.py
+++ ray_mapreduce/mapreduce.py
@@ -26,10 +26,11 @@ class Mapper(object):
 
     def get_new_article(self):
         # 获取文章内容
+        print(self.word_counts)
         article = wikipedia.page(self.title_stream.next()).content
+        print(article)
         # 分词&统计词频
         self.word_counts.append(Counter(re.split(r" |\n", article)))
-        print(self.word_counts)
         self.num_articles_processed += 1
 
     def get_range(self, article_index, keys):
