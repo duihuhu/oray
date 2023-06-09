0d1795ba1b25f57825f17f0a9654190a86e89b32
duihuhu <duihuhu@163.com>
2023-06-09 16:09:07 +0800

    debug add cudf ray actor

--- ray_gpu/cudf_task.py
+++ ray_gpu/cudf_task.py
@@ -9,7 +9,7 @@ print(t2-t1)
 
 
 t1=time.time()
-tips_df = cudf.read_csv("/home/hucc/cuda/cudf/tips.csv")
+tips_df = cudf.read_csv("/home/hucc/cuda/cudf/tips1.csv")
 tips_df['tip_percentage'] = tips_df['tip'] / tips_df['total_bill'] * 100
 result = tips_df.groupby('size').tip_percentage.mean()
 t2=time.time()
