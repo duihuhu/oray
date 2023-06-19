db02203862b8ba5136e8d0b0e6c1b5e8bd6b0a86
duihuhu <duihuhu@163.com>
2023-06-19 15:56:31 +0800

    debug add pycuda ray htd

---
+++ py_cuda/pycuda_ray.py
@@ -0,0 +1,33 @@
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+import numpy as np
+import ray
+import time
+ray.init()
+@ray.remote(num_gpus=0.5)
+def worker():
+  a = np.random.randn(4,4)
+  a = a.astype(np.float32)
+
+  a_gpu = cuda.mem_alloc(a.nbytes)
+
+  cuda.memcpy_htod(a_gpu, a)
+
+  mod = SourceModule("""
+  __global__ void doublify(float *a)
+  {
+  int idx = threadIdx.x + threadIdx.y*4;
+  a[idx] *= 2;
+  }
+  """)
+
+  func = mod.get_function("doublify")
+  func(a_gpu, block=(4,4,1))
+
+  a_doubled = np.empty_like(a)
+  cuda.memcpy_dtoh(a_doubled, a_gpu)
+  print(a_doubled)
+  print(a)
+
+print(worker.remote())
