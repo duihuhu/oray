f6f0ab6e418e5decf7e670ed29726188cbbc0e19
duihuhu <duihuhu@163.com>
2023-06-19 17:05:20 +0800

    debug find address in cudf

--- ray_gpu/cudf_task.py
+++ ray_gpu/cudf_task.py
@@ -3,6 +3,7 @@ import cudf
 t1=time.time()
 tips_df = cudf.read_csv("/home/hucc/cuda/cudf/tips.csv")
 print(id(tips_df))
+print(len(tips_df))
 tips_df['tip_percentage'] = tips_df['tip'] / tips_df['total_bill'] * 100
 result = tips_df.groupby('size').tip_percentage.mean()
 t2=time.time()
