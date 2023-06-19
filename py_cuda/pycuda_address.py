863327d351fb90790a8686ff0c51936c8ca75ffa
duihuhu <duihuhu@163.com>
2023-06-19 15:45:46 +0800

    debug add pycuda test

--- py_cuda/pycuda_address.py
+++ py_cuda/pycuda_address.py
@@ -3,5 +3,5 @@ import pycuda.driver as cuda
 print(pycuda.driver.mem_get_info())
 for i in range(10):
     d_A = cuda.mem_alloc(100)
-
+    print(pycuda.driver.mem_get_ipc_handle(d_A))
     print (int(d_A))
