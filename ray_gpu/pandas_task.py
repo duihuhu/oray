ccd0d5eae3edd897f1387edbd57ca04049ba743e
duihuhu <duihuhu@163.com>
2023-06-19 17:20:26 +0800

    debug find address in  pandas

--- ray_gpu/pandas_task.py
+++ ray_gpu/pandas_task.py
@@ -4,42 +4,8 @@ import time
 import pandas as pd
 t1=time.time()
 tips_df = pd.read_csv("/home/hucc/cuda/cudf/tips.csv")
-tips_df['tip_percentage'] = tips_df['tip'] / tips_df['total_bill'] * 100
-result = tips_df.groupby('size').tip_percentage.mean()
-t2=time.time()
-print(t2-t1)
-
-
-t1=time.time()
-tips_df = pd.read_csv("/home/hucc/cuda/cudf/tips.csv")
-tips_df['tip_percentage'] = tips_df['tip'] / tips_df['total_bill'] * 100
-result = tips_df.groupby('size').tip_percentage.mean()
-t2=time.time()
-print(t2-t1)
-
-t1=time.time()
-tips_df = pd.read_csv("/home/hucc/cuda/cudf/tips.csv")
-tips_df['tip_percentage'] = tips_df['tip'] / tips_df['total_bill'] * 100
-result = tips_df.groupby('size').tip_percentage.mean()
-t2=time.time()
-print(t2-t1)
-
-t1=time.time()
-tips_df = pd.read_csv("/home/hucc/cuda/cudf/tips.csv")
-tips_df['tip_percentage'] = tips_df['tip'] / tips_df['total_bill'] * 100
-result = tips_df.groupby('size').tip_percentage.mean()
-t2=time.time()
-print(t2-t1)
-
-t1=time.time()
-tips_df = pd.read_csv("/home/hucc/cuda/cudf/tips.csv")
-tips_df['tip_percentage'] = tips_df['tip'] / tips_df['total_bill'] * 100
-result = tips_df.groupby('size').tip_percentage.mean()
-t2=time.time()
-print(t2-t1)
-
-t1=time.time()
-tips_df = pd.read_csv("/home/hucc/cuda/cudf/tips.csv")
+print(id(tips_df))
+print(len(tips_df))
 tips_df['tip_percentage'] = tips_df['tip'] / tips_df['total_bill'] * 100
 result = tips_df.groupby('size').tip_percentage.mean()
 t2=time.time()
