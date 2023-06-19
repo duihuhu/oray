67fa95ce07629a94de7fd1f8a9a7b78f464ba4d7
duihuhu <duihuhu@163.com>
2023-06-19 17:48:05 +0800

    debug find address in  pandas

--- py_cuda/pycuda_address.py
+++ py_cuda/pycuda_address.py
@@ -2,4 +2,4 @@ import pycuda.autoinit
 import pycuda.driver as cuda
 for i in range(10):
     d_A = cuda.mem_alloc(100)
-    print(d_A, " " , id(d_A), " ", d_A.ptr)
+    print(d_A, " " , id(d_A), " ", int(d_A))
